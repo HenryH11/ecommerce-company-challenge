016f9e7f664b9c13ff7643188fd3de7383426d80
Henry H <hegohenry01@gmail.com>
2026-07-12 20:24:06 -0600

    Project structure created

---
+++ src/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("===================================")
+    print("eCommerce Company Challenge")
+    print("AI Knowledge Assistant")
+    print("===================================")
+
+
+if __name__ == "__main__":
+    main()
